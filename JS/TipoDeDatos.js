8f14ded170d3144fe852203a8694ab1155043d49
Valentina Iglesias <valentinaiglesiasg@hotmail.com>
2016-06-21 13:30:51 -0400

    agregado de tipos de datos

--- JS/TipoDeDatos.js
+++ JS/TipoDeDatos.js
@@ -1,6 +1,36 @@
-var numero1 = prompt("Ingrese el valor:");
-var numero2 = prompt("Ingrese el segundo valor:")
-var resultado = (numero1 / numero2);
+//ejercicio 1
+var num1, num2, resultado;
+do{
+	num1 = parseInt(prompt("Ingresar primer numero:" ));
+}while(isNaN( num1));
+
+do{
+	num2 = parseInt(prompt("Ingresar segundo numero:" ));
+}while(num2 == 0 || isNaN(num2));
+
+resultado = (num1/num2);
+
 alert(resultado);
 
+//ejercicio 2
+var nombre, edad, respuesta;
+nombre = prompt("Ingrese su nombre:");
+do{
+	edad = prompt("Ingrese su edad");
+}while(isNaN(edad));
+resultado ="Su nombre es " + nombre + " y a vivido " + (edad * 365) + " dias";
+alert(resultado);
 
+//ejercicio 3
+var nombre, apellido, poblacion, output;
+nombre = prompt("Ingrese su primer nombre : ");
+apellido = prompt("Ingrese su primer apellido : ");
+poblacion = prompt("Ingrese de la poblacion de la cual proviene : ");
+output = nombre + apellido + poblacion;
+alert(output);
+
+//ejercicio 4
+var valorx, valor;
+valorx = parseInt(prompt("Ingrese el numero"));
+valor = "Las sumas son: " + (valorx + 5) + "" + (valorx + 10)+ ""  + (valorx + 15);
+alert(valor);
